8eff717f002b5ca9e35a0c24423e2deb1d873e29
Ryan Smith <0ryansmith1994@gmail.com>
2015-06-04 14:18:16 +0100

    Moves code back to classes directory.

--- classes/log/store.php
+++ classes/log/store.php
@@ -1,187 +1,91 @@
-<?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+<?php namespace logstore_emitter\log;
+use \tool_log\log\writer as log_writer;
+use \tool_log\log\manager as log_manager;
+use \tool_log\helper\store as helper_store;
+use \tool_log\helper\reader as helper_reader;
+use \tool_log\helper\buffered_writer as helper_writer;
+use \core\event\base as event_base;
+use \logstore_emitter\xapi\service as xapi_service;
+use \logstore_emitter\xapi\repository as xapi_repository;
+use \logstore_emitter\moodle\service as moodle_service;
+use \logstore_emitter\moodle\repository as moodle_repository;
+use \moodle_exception as moodle_exception;
+use \stdClass as php_obj;
 
-/**
- * Standard log reader/writer.
- *
- * @package    logstore_xapi
- * @copyright  2015 Jerrett Fowler {@link http://charitylearning.org}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+class store extends php_obj implements log_writer {
+    use helper_store;
+    use helper_reader;
+    use helper_writer;
 
-namespace logstore_xapi\log;
-
-defined('MOODLE_INTERNAL') || die();
-
-require_once($CFG->dirroot . '/admin/tool/log/store/xapi/locallib.php');
-spl_autoload_register(function($class){
-    global $CFG;
-    $verbs = $CFG->dirroot . '/admin/tool/log/store/xapi/classes/xapi/verbs/';
-    foreach(glob($verbs . "*.php") as $file) {
-        require_once($file);
-    }
-});
-
-class store implements \tool_log\log\writer, \core\log\sql_internal_reader {
-    use \tool_log\helper\store,
-        \tool_log\helper\reader,
-        \tool_log\helper\buffered_writer;
-
-    protected $lrs;
-    protected $endpoint;
-    protected $username;
-    protected $password;
-
-    public function __construct(\tool_log\log\manager $manager) {
+    /**
+     * Constructs a new store.
+     * @param log_manager $manager
+     */
+    public function __construct(log_manager $manager) {
         $this->helper_setup($manager);
-        $this->endpoint = $this->get_config('endpoint', '');
-        $this->username = $this->get_config('username', '');
-        $this->password = $this->get_config('password', '');
-    }
-
-    protected function init() {
-        //Set up the connection to the LRS for testing purposes
-        if (isset($this->endpoint)) {
-            return !empty($this->endpoint);
-        }
-        try {
-            $lrs = lrs_connect($endpoint, $username, $password);
-            //Connect and test if LRS is available for sending
-        } catch (\moodle_exception $e) {
-            debugging('Cannot connect to LRS: ' . $e->getMessage(), DEBUG_DEVELOPER);
-            return false;
-        }
-        return true;
     }
 
     /**
-     * Should the event be ignored (== not logged)?
-     * @param \core\event\base $event
+     * Should the event be ignored (not logged)?
+     * @param event_base $event
      * @return bool
+     * @override helper_writer
      */
-    protected function is_event_ignored(\core\event\base $event) {
-        $verbs_available = $this->get_verb_classes();
-        foreach($verbs_available as $verb) {
-            if($this->validate_logstore_eventname($verb, $event->eventname)) {
-                return false;
-            }
-        }
-        return true;
+    protected function is_event_ignored(event_base $event) {
+        return isset(xapi_service::$action_to_recipe[$event['eventname']]);
     }
 
     /**
      * Insert events in bulk to the database.
-     *
      * @param array $evententries raw event data
+     * @override helper_writer
      */
-    protected function insert_event_entries($evententries) {
-        //This is where it will send to LRS
+    protected function insert_event_entries(array $events) {
         echo '<pre>', print_r($evententries, true), '</pre>';
-        if (!$this->is_logging()) {
-            return;
+
+        // Initializes required services.
+        $xapi_service = new xapi_service($this->connect_xapi_repository());
+        $moodle_service = new moodle_service($this->connect_moodle_repository());
+
+        // Emits events to other APIs.
+        foreach($events as $event) {
+            $moodle_event = $this->moodle_service->create($entry);
+            $xapi_statement = $this->xapi_service->create($moodle_event);
         }
-        try {
-            //Send xAPI statement to LRS
-            foreach($evententries as $entry) {
-                $verb_class = $this->get_verb_object($entry['action']);
-                if($verb_class){
-                    //create statement with new class
-                    $response = \logstore_xapi\set_record($entry, $verb_class->getID());
-                    //if($response->code == '400') //error
-                    //if($response->code == '200') //error
-                }
-            }
-        } catch (\moodle_exception $e) {
-            debugging('Cannot write to LRS: ' . $e->getMessage(), DEBUG_DEVELOPER);
-        }
-    }
-
-    public function get_events_select($selectwhere, array $params, $sort, $limitfrom, $limitnum) {
-        //Return events from the LRS given the parameters.
-        return false;
-    }
-
-    public function get_events_select_count($selectwhere, array $params) {
-        //Return a count of events in the LRS given the parameters.
-        return false;
-    }
-
-    public function get_internal_log_table_name() {
-        return false;
     }
 
     /**
-     * Are the new events appearing in the reader?
-     *
-     * @return bool true means new log events are being added, false means no new data will be added
+     * Determines if a connection exists to the store.
+     * @return boolean
      */
     public function is_logging() {
-        if (!$this->init()) {
+        try {
+            $this->connect_xapi_repository();
+            return true;
+        } catch (moodle_exception $ex) {
+            debugging('Cannot connect to LRS: ' . $e->getMessage(), DEBUG_DEVELOPER);
             return false;
         }
-        return true;
     }
 
     /**
-     * Get a list of available verbs.
+     * Creates a connection the xAPI store.
+     * @return xapi_repository
      */
-    protected function get_verb_classes() {
-        global $CFG;
-        $classes = array();
-        $basepath = $CFG->dirroot . "/admin/tool/log/store/xapi/classes/xapi/verbs/";
-        foreach(glob($basepath . "*.php") as $file) {
-            $classes[] = $file;
-        }
-        return $classes;
+    private function connect_xapi_repository() {
+        return new xapi_repository(
+            $this->get_config('endpoint', ''),
+            $this->get_config('username', ''),
+            $this->get_config('password', '')
+        );
     }
 
     /**
-     * Return a verb object if verb exists.
+     * Creates a connection the xAPI store.
+     * @return moodle_repository
      */
-    protected function get_verb_object($newverb) {
-        if(is_string($newverb) && strpos($newverb,'.php') !== false){
-            $newverb = basename($newverb, ".php");
-        }
-        if(is_object($newverb)){
-            return $newverb;
-        }
-        $availableverbs = $this->get_verb_classes();
-        foreach ($availableverbs as $verb) {
-            $verb = basename($verb, ".php");
-            if(!class_exists($verb)) {
-                if($verb == $newverb) {
-                    $new_verb_class = "logstore_xapi\\xapi\\verbs\\" . $newverb;
-                    return new $new_verb_class;
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Check if the logstore event name matches the allowed event names within the verb sub classes.
-     */
-    protected function validate_logstore_eventname($verbclass, $eventname) {
-        $class = $this->get_verb_object($verbclass);
-        if($class) {
-            $allowed_event_name = $class->getEventName($eventname);
-            if($eventname == $allowed_event_name){
-                return true;
-            }
-        }
-        return false;
+    private function connect_moodle_repository() {
+        global $DB;
+        return new moodle_repository($DB);
     }
 }
