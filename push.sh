eae7b806a32934a2f2dbb94b39c8765ea6c5f0af
Ryan Smith <0ryansmith1994@gmail.com>
2015-07-01 11:06:40 +0100

    Improves Travis.

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-git push "https://${GH_TOKEN}@${GH_REF}" ${TRAVIS_BRANCH}:${TRAVIS_BRANCH}
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" ${TRAVIS_BRANCH}:${TRAVIS_BRANCH} > /dev/null 2>&1
