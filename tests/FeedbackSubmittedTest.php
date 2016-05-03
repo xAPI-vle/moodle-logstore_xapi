eabeb8f5887a54c16c42b61361d27d2e7c1fdcd2
Andrew Downes <mrdownes@hotmail.com>
2016-05-03 17:12:00 +0100

    extend TestCase

--- tests/FeedbackSubmittedTest.php
+++ tests/FeedbackSubmittedTest.php
@@ -1,6 +1,6 @@
 <?php namespace Tests;
 
-class FeedbackSubmitted extends AttemptTestCase {
+class FeedbackSubmitted extends TestCase {
     protected function constructInput() {
         return array_merge(parent::constructInput(), [
             'eventname' => '\mod_feedback\event\response_submitted',
