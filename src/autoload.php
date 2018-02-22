ef3ddf82398636fd583084fb16f0b71c9472552f
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-22 17:42:42 +0000

    Corrects array.

--- src/autoload.php
+++ src/autoload.php
@@ -2,7 +2,7 @@
 
 $directory = new \RecursiveDirectoryIterator(__DIR__);
 $iterator = new \RecursiveIteratorIterator($directory);
-$files = array();
+$files = [];
 foreach ($iterator as $info) {
     $path_name = $info->getPathname();
     if (substr($path_name, -4) === '.php' && $path_name != __FILE__) {
