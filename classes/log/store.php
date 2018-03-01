edf308e4ea88682f81a13eb5df4e86d93f8e1041
Ryan Smith <0ryansmith1994@gmail.com>
2018-03-01 17:33:45 +0000

    Begins to update the store.php file.

--- classes/log/store.php
+++ classes/log/store.php
@@ -26,21 +26,13 @@
 namespace logstore_xapi\log;
 defined('MOODLE_INTERNAL') || die();
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../src/autoload.php');
 use \tool_log\log\writer as log_writer;
 use \tool_log\log\manager as log_manager;
 use \tool_log\helper\store as helper_store;
 use \tool_log\helper\reader as helper_reader;
 use \tool_log\helper\buffered_writer as helper_writer;
 use \core\event\base as event_base;
-use \XREmitter\Controller as xapi_controller;
-use \XREmitter\Repository as xapi_repository;
-use \MXTranslator\Controller as translator_controller;
-use \MXTranslator\Events\Event as Event;
-use \LogExpander\Controller as moodle_controller;
-use \LogExpander\Repository as moodle_repository;
-use \TinCan\RemoteLRS as tincan_remote_lrs;
-use \moodle_exception as moodle_exception;
 use \stdClass as php_obj;
 
 /**
@@ -103,7 +95,7 @@ class store extends php_obj implements log_writer {
         global $DB;
 
         // If in background mode, just save them in the database.
-        if (get_config('logstore_xapi', 'backgroundmode')) {
+        if ($this->get_config('backgroundmode', false)) {
             $DB->insert_records('logstore_xapi_log', $events);
         } else {
             $this->process_events($events);
@@ -111,85 +103,28 @@ class store extends php_obj implements log_writer {
     }
 
     public function process_events(array $events) {
-
-        // Initializes required services.
-        $xapicontroller = new xapi_controller($this->connect_xapi_repository());
-        $moodlecontroller = new moodle_controller($this->connect_moodle_repository());
-        $translatorcontroller = new translator_controller();
-
-        // Emits events to other APIs.
-        foreach ($events as $index => $event) {
-            $events[$index] = (array) $event;
-        }
-
-        $this->error_log('');
-        $this->error_log_value('events', $events);
-        $moodleevents = $moodlecontroller->create_events($events);
-
-        // Clear the user email if mbox setting is not set to mbox.
-        $mbox = get_config('logstore_xapi', 'mbox');
-        foreach (array_keys($moodleevents) as $eventkey) {
-            $moodleevents[$eventkey]['sendmbox'] = $mbox;
-        }
-
-        $this->error_log_value('moodleevent', $moodleevents);
-        $translatorevents = $translatorcontroller->create_events($moodleevents);
-        $this->error_log_value('translatorevents', $translatorevents);
-
-        if (empty($translatorevents)) {
-            return [];
-        }
-
-        // Split statements into batches.
-        $eventbatches = array($translatorevents);
-        $maxbatchsize = get_config('logstore_xapi', 'maxbatchsize');
-
-        if (!empty($maxbatchsize) && $maxbatchsize < count($translatorevents)) {
-            $eventbatches = array_chunk($translatorevents, $maxbatchsize);
-        }
-
-        $translatorevent = new Event();
-        $translatoreventreadreturn = @$translatorevent->read([]);
-
-        $sentevents = [];
-        foreach ($eventbatches as $translatoreventsbatch) {
-            $xapievents = $xapicontroller->create_events($translatoreventsbatch);
-            $statements = $xapievents['statements'];
-            $response = $xapievents['response'];
-            foreach (array_keys($statements) as $key) {
-                if (is_numeric($key)) {
-                    $k = $statements[$key]['context']['extensions'][$translatoreventreadreturn[0]['context_ext_key']]['id'];
-                    $sentevents[$k] = $this->getlast_action_result($response);
-                }
-            }
-            $this->error_log_value('xapievents', $xapievents);
-        }
-
-        return $sentevents;
-    }
-
-    /**
-     * Get last action result from Learning Locker.
-     * @param Object TinCan\LRSResponse
-     *
-     */
-    private function getlast_action_result($response) {
-        if ($response->success == 1) {
-            return "success";
-        } else {
-            return "failure";
-        }
-    }
-
-    private function error_log_value($key, $value) {
-        $this->error_log('['.$key.'] '.json_encode($value));
-    }
-
-    private function error_log($message) {
-        if ($this->loggingenabled) {
-            // @codingStandardsIgnoreLine
-            error_log($message."\r\n", 3, __DIR__.'/error_log.txt');
-        }
+        global $DB;
+        global $CFG;
+        $handler_config = [
+            'transformer' => [
+                'source_url' => 'http://moodle.org',
+                'source_name' => 'Moodle',
+                'source_version' => '1.0.0',
+                'source_lang' => 'en',
+                'send_mbox' => $this->get_config('mbox', false),
+                'plugin_url' => 'http://www.example.org/plugin',
+                'plugin_version' => '1.0.0',
+                'repo' => new moodle_repository($DB, $CFG),
+            ],
+            'loader' => [
+                'loader' => 'log',
+                'lrs_endpoint' => $this->get_config('endpoint', ''),
+                'lrs_username' => $this->get_config('username', ''),
+                'lrs_password' => $this->get_config('password', ''),
+                'lrs_max_batch_size' => $this->get_config('maxbatchsize', 100),
+            ],
+        ];
+        \src\handler($handler_config, $events);
     }
 
     /**
@@ -197,40 +132,6 @@ class store extends php_obj implements log_writer {
      * @return boolean
      */
     public function is_logging() {
-        try {
-            $this->connect_xapi_repository();
-            return true;
-        } catch (moodle_exception $ex) {
-            debugging('Cannot connect to LRS: ' . $ex->getMessage(), DEBUG_DEVELOPER);
-            return false;
-        }
-    }
-
-    /**
-     * Creates a connection the xAPI store.
-     * @return xapi_repository
-     */
-    private function connect_xapi_repository() {
-        global $CFG;
-        $remotelrs = new tincan_remote_lrs(
-            $this->get_config('endpoint', ''),
-            '1.0.1',
-            $this->get_config('username', ''),
-            $this->get_config('password', '')
-        );
-        if (!empty($CFG->proxyhost)) {
-            $remotelrs->setProxy($CFG->proxyhost.':'.$CFG->proxyport);
-        }
-        return new xapi_repository($remotelrs);
-    }
-
-    /**
-     * Creates a connection the xAPI store.
-     * @return moodle_repository
-     */
-    private function connect_moodle_repository() {
-        global $DB;
-        global $CFG;
-        return new moodle_repository($DB, $CFG);
+        return true;
     }
 }
