218ab1033808031e9673a524695472024a0da32b
Ryan Smith <0ryansmith1994@gmail.com>
2018-03-21 16:31:33 +0000

    Adds tests for assignment_submitted.

--- src/transformer/events/mod_assign/assignment_submitted.php
+++ src/transformer/events/mod_assign/assignment_submitted.php
@@ -4,10 +4,10 @@ namespace transformer\events\mod_assign;
 
 use transformer\utils as utils;
 
-function assignment_submitted(array $config, array $event) {
+function assignment_submitted(array $config, \stdClass $event) {
     $repo = $config['repo'];
-    $user = $repo->read_user($event['userid']);
-    $course = $repo->read_course($event['courseid']);
+    $user = $repo->read_record_by_id('user', $event->userid);
+    $course = $repo->read_record_by_id('course', $event->courseid);
     $lang = utils\get_course_lang($course);
 
     return [[
@@ -18,7 +18,7 @@ function assignment_submitted(array $config, array $event) {
                 $lang => 'submitted'
             ],
         ],
-        'object' => utils\get_module_activity($config, $event, $lang),
+        'object' => utils\get_activity\module($config, $event, $lang),
         'timestamp' => utils\get_event_timestamp($event),
         'context' => [
             'platform' => $config['source_name'],
@@ -28,10 +28,11 @@ function assignment_submitted(array $config, array $event) {
             ],
             'contextActivities' => [
                 'grouping' => [
-                    utils\get_course_activity($course)
+                    utils\get_activity\site($config),
+                    utils\get_activity\course($config, $course),
                 ],
                 'category' => [
-                    utils\get_source_activity($config)
+                    utils\get_activity\source($config)
                 ]
             ],
         ]
