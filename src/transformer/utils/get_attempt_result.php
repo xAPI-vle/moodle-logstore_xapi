ef9386cd5058cfc0838d8b23af331bd97e8efb7b
Lee Kirkland <lee.a.kirkland@associates.ice.dhs.gov>
2018-07-02 13:03:47 -0400

    correcting ternary since ?: means sent blank if true

--- src/transformer/utils/get_attempt_result.php
+++ src/transformer/utils/get_attempt_result.php
@@ -5,9 +5,9 @@ namespace src\transformer\utils;
 function get_attempt_result(array $config, $attempt, $grade_item) {
     $grade_sum = isset($attempt->sumgrades) ? $attempt->sumgrades : 0;
 
-    $min_score = (float) ($grade_item->grademin ?: 0);
-    $max_score = (float) ($grade_item->grademax ?: 0);
-    $pass_score = (float) ($grade_item->gradepass ?: null);
+    $min_score = (float)($grade_item->grademin ? $grade_item->grademin : 0);
+    $max_score = (float)($grade_item->grademax ? $grade_item->grademax : 0);
+    $pass_score = (float)($grade_item->gradepass ? $grade_item->gradepass : null);
 
     $raw_score = cap_raw_score($grade_sum, $min_score, $max_score);
     $scaled_score = get_scaled_score($raw_score, $min_score, $max_score);
