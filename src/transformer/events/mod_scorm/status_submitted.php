f25738636d63b2a4690bbdb0230a56dec49e4c8f
Lydia Ross <lydiaross@Lydias-MacBook-Air.local>
2018-07-03 14:01:38 +0100

    fix: scorm status submitted

--- src/transformer/events/mod_scorm/status_submitted.php
+++ src/transformer/events/mod_scorm/status_submitted.php
@@ -8,12 +8,13 @@ function status_submitted(array $config, \stdClass $event) {
     $repo = $config['repo'];
     $user = $repo->read_record_by_id('user', $event->userid);
     $course = $repo->read_record_by_id('course', $event->courseid);
+    $scorm = $repo->read_record_by_id('scorm', $event->objectid);
     $lang = utils\get_course_lang($course);
 
     $unserialized_cmi = unserialize($event->other);
     $attempt = $unserialized_cmi['attemptid'];
     $scorm_scoes_tracks = $repo->read_records('scorm_scoes_track', [
-        'userid' => $userid,
+        'userid' => $user->id,
         'scormid' => $event->objectid,
         'scoid' => $event->contextinstanceid,
         'attempt' => $unserialized_cmi['attemptid']
@@ -22,7 +23,7 @@ function status_submitted(array $config, \stdClass $event) {
     return [[
         'actor' => utils\get_user($config, $user),
         'verb' => utils\get_scorm_verb($scorm_scoes_tracks, $lang),
-        'object' => utils\get_activity\event_module($config, $event, $lang),
+        'object' => utils\get_activity\module($config, 'scorm', $scorm, $lang),
         'timestamp' => utils\get_event_timestamp($event),
         'context' => [
             'platform' => $config['source_name'],
