47951e6f45dab87f43d8e6262f1fe173be84392b
Lee Kirkland <lee.a.kirkland@associates.ice.dhs.gov>
2018-07-03 10:57:56 -0400

    trying to fix raw score in attempt_submitted and add question

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -36,6 +36,7 @@ function get_event_function_map() {
         '\mod_quiz\event\attempt_started' => 'mod_quiz\attempt_started',
         '\mod_quiz\event\attempt_reviewed' => 'mod_quiz\attempt_reviewed',
         '\mod_quiz\event\attempt_submitted' => 'mod_quiz\attempt_submitted',
+        '\mod_quiz\event\question_submitted' => 'mod_quiz\question_submitted',
         '\mod_quiz\event\attempt_viewed' => 'all\course_module_viewed',
         '\mod_resource\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_scorm\event\course_module_viewed' => 'all\course_module_viewed',
--- src/transformer/utils/get_attempt_result.php
+++ src/transformer/utils/get_attempt_result.php
@@ -9,8 +9,8 @@ function get_attempt_result(array $config, $attempt, $grade_item) {
     $max_score = (float)($grade_item->grademax ? $grade_item->grademax : 0);
     $pass_score = (float)($grade_item->gradepass ? $grade_item->gradepass : null);
 
-    $raw_score = cap_raw_score($grade_sum, $min_score, $max_score);
-    $scaled_score = get_scaled_score($raw_score, $min_score, $max_score);
+    $raw_score = floatval((cap_raw_score($grade_sum, $min_score, $max_score)));
+    $scaled_score = floatval((get_scaled_score($raw_score, $min_score, $max_score)));
 
     $completed = isset($attempt->state) ? $attempt->state === 'finished' : false;
     $success = $grade_sum >= $pass_score;
