c68a7ec706635cb99f58627b48a2a51452426b09
Lydia Ross <lydiaross@Lydias-MacBook-Air.local>
2018-07-03 16:38:19 +0100

    fix: scorm raw score submitted

--- src/transformer/utils/get_scaled_score.php
+++ src/transformer/utils/get_scaled_score.php
@@ -3,7 +3,9 @@
 namespace src\transformer\utils;
 
 function get_scaled_score($raw_score, $min_score, $max_score) {
-    $score_range = $max_score - $min_score;
-    $score = $raw_score - $min_score;
-    return $score / $score_range;
+    if ($raw_score >= 0) {
+        return $raw_score / $max_score;
+    } else {
+        return $raw_score / $min_score;
+    }
 }
--- src/transformer/utils/get_scorm_result.php
+++ src/transformer/utils/get_scorm_result.php
@@ -8,20 +8,45 @@ function get_scorm_result($scorm_scoes_tracks, $raw_score) {
 
     foreach ($scorm_scoes_tracks as $st) {
         if ($st->element == 'cmi.core.score.min') {
-            $min_score = $st->value;
+            $min_score = floatval($st->value);
         } else if ($st->element == 'cmi.core.score.max') {
-            $max_score = $st->value;
+            $max_score = floatval($st->value);
         }
     }
     
-    $scaled_score = get_scaled_score($raw_score, $min_score, $max_score);
+    if ($max_score !== null && $min_score !== null) {
+        $scaled_score = get_scaled_score($raw_score, $min_score, $max_score);
+        return [
+            'score' => [
+                'raw' => $raw_score,
+                'min' => $min_score,
+                'max' => $max_score,
+                'scaled' => $scaled_score,
+            ],
+        ];
+    }
+
+    if ($max_score !== null && $min_score === null) {
+        return [
+            'score' => [
+                'raw' => $raw_score,
+                'max' => $max_score,
+            ],
+        ];
+    }
+
+    if ($max_score === null && $min_score !== null) {
+        return [
+            'score' => [
+                'raw' => $raw_score,
+                'min' => $min_score,
+            ],
+        ];
+    }
 
     return [
         'score' => [
             'raw' => $raw_score,
-            'min' => $min_score,
-            'max' => $max_score,
-            'scaled' => $scaled_score
         ],
     ];
 }
