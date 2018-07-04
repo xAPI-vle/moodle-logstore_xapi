95db72f2ec25ede427f20bea6b42801fd070f21c
Lydia Ross <lydiaross@Lydias-MacBook-Air.local>
2018-07-04 15:20:48 +0100

    fix: course completed event

--- src/transformer/events/core/course_completed.php
+++ src/transformer/events/core/course_completed.php
@@ -6,7 +6,7 @@ use src\transformer\utils as utils;
 
 function course_completed(array $config, \stdClass $event) {
     $repo = $config['repo'];
-    $user = $repo->read_record_by_id('user', $event->userid);
+    $user = $repo->read_record_by_id('user', $event->relateduserid);
     $course = $repo->read_record_by_id('course', $event->courseid);
     $lang = utils\get_course_lang($course);
 
