69b6cdebef36ddf49e7dbf8c5892b4bb792e4b21
Lee Kirkland <lee.a.kirkland@associates.ice.dhs.gov>
2018-07-11 14:06:44 -0400

    changed variable name to get inline with calling functions

--- src/transformer/utils/get_module_name.php
+++ src/transformer/utils/get_module_name.php
@@ -2,12 +2,12 @@
 
 namespace src\transformer\utils;
 
-function get_module_name(array $config, $module_type, $module) {
+function get_module_name(array $config, $moduletype, $module) {
     $name = '';
     if (property_exists($module, 'name')) {
         $name = $module->name;
-    } else if (property_exists($module_type, 'name')) {
-        $name = $module_type->name;
+    } else if (property_exists($moduletype, 'name')) {
+        $name = $moduletype->name;
     }
     // If name has still not been set, then use event functions, and set that as name to avoid error.
     if ($name == '' && array_key_exists('event_function', $config)) {
