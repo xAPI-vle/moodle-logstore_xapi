6cfa74a25043ad9b4ea4d98c493c295e3ada4540
Ryan Smith <0ryansmith1994@gmail.com>
2018-07-30 13:10:26 +0100

    test: Adds test for quiz without questions.

--- tests/mod_quiz/attempt_submitted/no_questions/test.php
+++ tests/mod_quiz/attempt_submitted/no_questions/test.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace tests\mod_quiz\attempt_submitted\essay;
+namespace tests\mod_quiz\attempt_submitted\no_questions;
 defined('MOODLE_INTERNAL') || die();
 
 class test extends \tests\xapi_test_case {
