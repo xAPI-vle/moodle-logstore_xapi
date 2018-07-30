99c930986152bf66fa0cf01d8493925398293588
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-07-30 17:47:41 +0100

    feat: Adds statement for shortanswer quiz questions. (#199)

--- src/transformer/events/mod_quiz/question_answered/handler.php
+++ src/transformer/events/mod_quiz/question_answered/handler.php
@@ -29,6 +29,8 @@ function handler(array $config, \stdClass $event, \stdClass $questionattempt) {
             return essay($config, $event, $questionattempt, $question);
         case 'truefalse':
             return truefalse($config, $event, $questionattempt, $question);
+        case 'shortanswer':
+            return shortanswer($config, $event, $questionattempt, $question);
         case 'multichoice':
             return multichoice($config, $event, $questionattempt, $question);
         case 'numerical':
