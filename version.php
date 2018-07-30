6006e4b6c0fed0f897e1668130c7544c543b3c51
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-07-30 17:46:14 +0100

    fix: Fixes cron errors reported by @funclickearn_twitter. (#202)

--- version.php
+++ version.php
@@ -16,9 +16,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin = isset($plugin) && is_object($plugin) ? $plugin : new StdClass();
+$plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2018061100;
-$plugin->release = '0.0.0-development';
+$plugin->version = 2018073005;
+$plugin->release = '';
 $plugin->requires = 2014111000;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
