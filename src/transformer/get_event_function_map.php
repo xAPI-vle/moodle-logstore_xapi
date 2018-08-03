852fc52667c94cd014905c08c55ce714aa78c07f
Ryan Smith <0ryansmith1994@gmail.com>
2018-08-03 14:04:04 +0100

    fix: Fixes course_module_viewed activity definition type for survey.

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -58,7 +58,7 @@ function get_event_function_map() {
         '\mod_scorm\event\sco_launched' => 'mod_scorm\sco_launched',
         '\mod_scorm\event\scoreraw_submitted' => 'mod_scorm\scoreraw_submitted',
         '\mod_scorm\event\status_submitted' => 'mod_scorm\status_submitted',
-        '\mod_survey\event\course_module_viewed' => 'all\course_module_viewed',
+        '\mod_survey\event\course_module_viewed' => 'mod_survey\course_module_viewed',
         '\mod_url\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_wiki\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_workshop\event\course_module_viewed' => 'all\course_module_viewed',
