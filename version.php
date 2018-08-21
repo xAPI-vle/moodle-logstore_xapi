37e3c11b4d0d282ab10566cbaf8bccf352baf99a
Ryan Smith <0ryansmith1994@gmail.com>
2018-08-21 15:00:59 +0100

    ci(savepoints): Attempts to fix savepoints.

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2018073005;
+$plugin->version = 2018082100;
 $plugin->release = '';
 $plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_STABLE;
