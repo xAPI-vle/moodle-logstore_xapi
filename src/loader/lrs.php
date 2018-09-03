66df44e67b3a88cec0f4f1aeede5ce79f2d292f5
Ryan Smith <0ryansmith1994@gmail.com>
2018-09-03 13:29:48 +0100

    refactor: Splits lrs loaders into utils.

--- src/loader/lrs.php
+++ src/loader/lrs.php
@@ -19,78 +19,38 @@ defined('MOODLE_INTERNAL') || die();
 
 use src\loader\utils as utils;
 
-function send_http_statements(array $config, array $statements) {
-    $endpoint = $config['lrs_endpoint'];
-    $username = $config['lrs_username'];
-    $password = $config['lrs_password'];
-    $proxyendpoint = $config['lrs_proxy_endpoint'];
-
-    $url = utils\correct_endpoint($endpoint).'/statements';
-    $auth = base64_encode($username.':'.$password);
-    $postdata = json_encode($statements);
-
-    $request = curl_init();
-    curl_setopt($request, CURLOPT_URL, $url);
-    curl_setopt($request, CURLOPT_POSTFIELDS, $postdata);
-    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
-    if (isset($proxyendpoint)) {
-        curl_setopt($request, CURLOPT_PROXY, $proxyendpoint);
-    }
-    curl_setopt($request, CURLOPT_HTTPHEADER, [
-        'Authorization: Basic '.$auth,
-        'X-Experience-API-Version: 1.0.0',
-        'Content-Type: application/json',
-    ]);
-
-    $responsetext = curl_exec($request);
-    $responsecode = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
-    curl_close($request);
-
-    if ($responsecode !== 200) {
-        throw new \Exception($responsetext);
-    }
-}
-
-function load_transormed_events_to_lrs(array $config, array $transformedevents) {
-    try {
-        $statements = array_reduce($transformedevents, function ($result, $transformedevent) {
-            $eventstatements = $transformedevent['statements'];
-            return array_merge($result, $eventstatements);
-        }, []);
-        send_http_statements($config, $statements);
-        $loadedevents = utils\construct_loaded_events($transformedevents, true);
-        return $loadedevents;
-    } catch (\Exception $e) {
-        $logerror = $config['log_error'];
-        $logerror("Failed load for event id #" . $eventobj->id . ": " .  $e->getMessage());
-        $logerror($e->getTraceAsString());
-        $loadedevents = utils\construct_loaded_events($transformedevents, false);
-        return $loadedevents;
-    }
-}
-
-function get_event_batches(array $config, array $transformedevents) {
-    $maxbatchsize = $config['lrs_max_batch_size'];
-    if (!empty($maxbatchsize) && $maxbatchsize < count($transformedevents)) {
-        return array_chunk($transformedevents, $maxbatchsize);
-    }
-    return [$transformedevents];
-}
-
 function load(array $config, array $events) {
-    // Attempts to load events that were transformed successfully in batches.
-    $successfultransformevents = utils\filter_transformed_events($events, true);
-    $batches = utils\get_event_batches($config, $successfultransformevents);
-    $loadedevents = array_reduce($batches, function ($result, $batch) use ($config) {
-        $loadedbatchevents = load_transormed_events_to_lrs($config, $batch);
-        return array_merge($result, $loadedbatchevents);
-    }, []);
+    $send_http_statements = function (array $config, array $statements) {
+        $endpoint = $config['lrs_endpoint'];
+        $username = $config['lrs_username'];
+        $password = $config['lrs_password'];
+        $proxyendpoint = $config['lrs_proxy_endpoint'];
+    
+        $url = utils\correct_endpoint($endpoint).'/statements';
+        $auth = base64_encode($username.':'.$password);
+        $postdata = json_encode($statements);
+    
+        $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
+        if (isset($proxyendpoint)) {
+            curl_setopt($request, CURLOPT_PROXY, $proxyendpoint);
+        }
+        curl_setopt($request, CURLOPT_HTTPHEADER, [
+            'Authorization: Basic '.$auth,
+            'X-Experience-API-Version: 1.0.0',
+            'Content-Type: application/json',
+        ]);
+    
+        $responsetext = curl_exec($request);
+        $responsecode = curl_getinfo($request, CURLINFO_RESPONSE_CODE);
+        curl_close($request);
 
-    // Flags events that weren't transformed successfully as events that didn't load.
-    $failedtransformevents = utils\filter_transformed_events($events, false);
-    $nonloadedevents = utils\construct_loaded_events($failedtransformevents, false);
-
-    // Returns loaded and non-loaded events to avoid re-processing.
-    return array_merge($loadedevents, $nonloadedevents);
+        if ($responsecode !== 200) {
+            throw new \Exception($responsetext);
+        }
+    };
+    return utils\load_in_batches($config, $events, $send_http_statements);
 }
--- src/loader/moodle_curl_lrs.php
+++ src/loader/moodle_curl_lrs.php
@@ -18,66 +18,37 @@ namespace src\loader\moodle_curl_lrs;
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
+if (!isset($CFG)) {
+    $CFG = (object) [ 'libdir' => 'utils' ];
+}
 require_once($CFG->libdir . '/filelib.php');
 
 use src\loader\utils as utils;
 
-function send_http_statements(array $config, array $statements) {
-    $endpoint = $config['lrs_endpoint'];
-    $username = $config['lrs_username'];
-    $password = $config['lrs_password'];
-    $proxyendpoint = $config['lrs_proxy_endpoint'];
-
-    $url = utils\correct_endpoint($endpoint).'/statements';
-    $auth = base64_encode($username.':'.$password);
-    $postdata = json_encode($statements);
-
-    $request = new \curl();
-    $responsetext = $request->post($url, $postdata, [
-        'CURLOPT_HTTPHEADER' => [
-            'Authorization: Basic '.$auth,
-            'X-Experience-API-Version: 1.0.0',
-            'Content-Type: application/json',
-        ],
-    ]);
-    $responsecode = $request->info['http_code'];
-
-    if ($responsecode !== 200) {
-        throw new \Exception($responsetext);
-    }
-}
-
-function load_transormed_events_to_lrs(array $config, array $transformedevents) {
-    try {
-        $statements = array_reduce($transformedevents, function ($result, $transformedevent) {
-            $eventstatements = $transformedevent['statements'];
-            return array_merge($result, $eventstatements);
-        }, []);
-        send_http_statements($config, $statements);
-        $loadedevents = utils\construct_loaded_events($transformedevents, true);
-        return $loadedevents;
-    } catch (\Exception $e) {
-        $logerror = $config['log_error'];
-        $logerror("Failed load for event id #" . $eventobj->id . ": " .  $e->getMessage());
-        $logerror($e->getTraceAsString());
-        $loadedevents = utils\construct_loaded_events($transformedevents, false);
-        return $loadedevents;
-    }
-}
-
 function load(array $config, array $events) {
-    // Attempts to load events that were transformed successfully in batches.
-    $successfultransformevents = utils\filter_transformed_events($events, true);
-    $batches = utils\get_event_batches($config, $successfultransformevents);
-    $loadedevents = array_reduce($batches, function ($result, $batch) use ($config) {
-        $loadedbatchevents = load_transormed_events_to_lrs($config, $batch);
-        return array_merge($result, $loadedbatchevents);
-    }, []);
-
-    // Flags events that weren't transformed successfully as events that didn't load.
-    $failedtransformevents = utils\filter_transformed_events($events, false);
-    $nonloadedevents = utils\construct_loaded_events($failedtransformevents, false);
-
-    // Returns loaded and non-loaded events to avoid re-processing.
-    return array_merge($loadedevents, $nonloadedevents);
+    $send_http_statements = function (array $config, array $statements) {
+        $endpoint = $config['lrs_endpoint'];
+        $username = $config['lrs_username'];
+        $password = $config['lrs_password'];
+        $proxyendpoint = $config['lrs_proxy_endpoint'];
+    
+        $url = utils\correct_endpoint($endpoint).'/statements';
+        $auth = base64_encode($username.':'.$password);
+        $postdata = json_encode($statements);
+    
+        $request = new \curl();
+        $responsetext = $request->post($url, $postdata, [
+            'CURLOPT_HTTPHEADER' => [
+                'Authorization: Basic '.$auth,
+                'X-Experience-API-Version: 1.0.0',
+                'Content-Type: application/json',
+            ],
+        ]);
+        $responsecode = $request->info['http_code'];
+    
+        if ($responsecode !== 200) {
+            throw new \Exception($responsetext);
+        }
+    };
+    return utils\load_in_batches($config, $events, $send_http_statements);
 }
