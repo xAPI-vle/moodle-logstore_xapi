dbec6bf7a83c335b32b7eac87eb5783624b55a62
Andrew Downes <mrdownes@hotmail.com>
2018-12-21 15:18:11 +0000

    refactor: Renames variable because statements were actually events. (#365)

--- src/loader/handler.php
+++ src/loader/handler.php
@@ -18,8 +18,8 @@ namespace src\loader;
 
 defined('MOODLE_INTERNAL') || die();
 
-function handler(array $config, array $statements) {
+function handler(array $config, array $events) {
     $loadername = $config['loader'];
     $load = "\src\loader\\$loadername\load";
-    return $load($config, $statements);
+    return $load($config, $events);
 }
