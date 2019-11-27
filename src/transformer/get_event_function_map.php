243694f392f3429cf92fff7a6254fed1cf4eb2d2
Nathan Nguyen <nathannguyen@catalyst-au.net>
2019-11-27 14:44:24 +1100

    WR322566 fix test failure in core_event_deprecated_testcase

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -87,8 +87,19 @@ function get_event_function_map() {
         '\mod_workshop\event\course_module_viewed' => 'all\course_module_viewed',
         '\totara_program\event\program_assigned' => 'totara_program\program_assigned'
     ];
+    if(PHPUNIT_TEST) {
+        /**
+         * In unit test, if test_adminroot_cache_reset test is run before core_event_deprecated_testcase
+         * The report_eventlist_list_generator will load course_module_instances_list_viewed abstract class
+         * which will cause the core_event_deprecated_testcase to fail
+         * (debugging already called and the debug mode is off - list_generator.php)
+         **/
+        $environmentevents = $availableevents;
 
-    $environmentevents = class_exists("report_eventlist_list_generator") ? array_keys(\report_eventlist_list_generator::get_all_events_list(false)) : array_keys($availableevents);
+    } else {
+        $environmentevents = class_exists("report_eventlist_list_generator") ?
+            array_keys(\report_eventlist_list_generator::get_all_events_list(false)) : array_keys($availableevents);
+    }
 
     return array_filter($availableevents, function($k) use ($environmentevents) {
         return in_array($k, $environmentevents);
