c9237e04c59d70ce73889a560bbd86c2137b0961
Michael Lynn <michael.lynn@learningpool.com>
2020-03-30 01:06:12 +0100

    LMS-1652: add capability tool/logstorexapi:viewerrorlog

---
+++ db/access.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of Moodle LMS
+ *
+ * Copyright (C) 2020 Learning Pool Ltd <http://learningpool.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Capability definitions
+ *
+ * @package    tool_logstorexapi
+ * @author     Michael Lynn <michael.lynn@learningpool.com>
+ * @copyright  2020 Learning Pool Ltd <http://learningpool.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+        'tool/logstorexapi:viewerrorlog' => array(
+                'riskbitmask'  => RISK_CONFIG,
+                'captype'      => 'read',
+                'contextlevel' => CONTEXT_SYSTEM,
+                'archetypes'   => array(
+                        'manager' => CAP_ALLOW
+                )
+        )
+);
