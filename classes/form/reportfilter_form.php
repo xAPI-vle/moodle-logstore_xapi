366b41f8603934bc6c754fda79af77b1150954fe
Stephen O'Hara <stephen.ohara@learningpool.com>
2020-04-06 16:19:15 +0100

    LMS-1650 - Error page filters skeleton

---
+++ classes/form/reportfilter_form.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once($CFG->libdir.'/formslib.php');
+
+/**
+ * The contact form to the site's Data Protection Officer
+ *
+ * @copyright 2020 onwards Stephen O'Hara
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package tool_logstore_xapi
+ */
+class tool_logstore_xapi_reportfilter_form extends moodleform {
+
+    /**
+     * Form definition.
+     */
+    public function definition() {
+
+    }
+}
