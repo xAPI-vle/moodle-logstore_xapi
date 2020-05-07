3f7486398e8d468aa797f76e46e659b21c58e031
Michael Lynn <michael.lynn@learningpool.com>
2020-05-07 23:43:42 +0100

    LMS-2172: add logstore_xapi_sent_log

--- db/upgrade.php
+++ db/upgrade.php
@@ -64,6 +64,60 @@ function create_xapi_log_table($dbman, $tablename) {
     }
 }
 
+function create_xapi_sent_log_table($dbman, $tablename) {
+    // Define table to be created.
+    $table = new xmldb_table($tablename);
+
+    // Adding fields to table.
+    $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+    $table->add_field('eventname', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('component', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('action', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('target', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('objecttable', XMLDB_TYPE_CHAR, '50', null, null, null, null);
+    $table->add_field('objectid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('crud', XMLDB_TYPE_CHAR, '1', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('edulevel', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('contextid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('contextlevel', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('contextinstanceid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('relateduserid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('anonymous', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0');
+    $table->add_field('other', XMLDB_TYPE_TEXT, null, null, null, null, null);
+    $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+    $table->add_field('origin', XMLDB_TYPE_CHAR, '10', null, null, null, null);
+    $table->add_field('ip', XMLDB_TYPE_CHAR, '45', null, null, null, null);
+    $table->add_field('realuserid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('errortype', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('response', XMLDB_TYPE_TEXT, null, null, null, null, null);
+    $table->add_field('logstorestandardlogid', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+    $table->add_field('type', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+
+    // Adding keys to table.
+    $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+    // Adding indexes to table.
+    $table->add_index('timecreated', XMLDB_INDEX_NOTUNIQUE, array('timecreated'));
+    $table->add_index('course-time', XMLDB_INDEX_NOTUNIQUE, array('courseid', 'anonymous', 'timecreated'));
+    $table->add_index('user-module', XMLDB_INDEX_NOTUNIQUE, array(
+        'userid',
+        'contextlevel',
+        'contextinstanceid',
+        'crud',
+        'edulevel',
+        'timecreated'
+    ));
+    $table->add_index('logstorestandardlogid', XMLDB_INDEX_NOTUNIQUE, array('logstorestandardlogid'));
+    $table->add_index('type', XMLDB_INDEX_NOTUNIQUE, array('type'));
+
+    // Conditionally launch create table.
+    if (!$dbman->table_exists($table)) {
+        $dbman->create_table($table);
+    }
+}
+
 /**
  * Create new columns in the database.
  * 
@@ -179,6 +233,14 @@ function xmldb_logstore_xapi_upgrade($oldversion) {
     if ($oldversion < 2020050100) {
         create_logstoreid_and_type_columns($dbman);
         upgrade_plugin_savepoint(true, 2020050100, 'logstore', 'xapi');
+    }
+
+    if ($oldversion < 2020050600) {
+
+        create_xapi_sent_log_table($dbman, "logstore_xapi_sent_log");
+
+        // Xapi savepoint reached.
+        upgrade_plugin_savepoint(true, 2020050600, 'logstore', 'xapi');
     }
 
     return true;
