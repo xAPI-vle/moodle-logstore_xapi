765143b34fe780bb695f04d5c1aee1a119f1a0c1
Michael Lynn <michael.lynn@learningpool.com>
2020-05-19 10:48:45 +0100

    LMS-1698: copyright message and require_once

--- cli/testdataseeder.php
+++ cli/testdataseeder.php
@@ -1,28 +1,44 @@
+
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 define('CLI_SCRIPT', 1);
-require(__DIR__.'/../../../../../../config.php');
+require_once(__DIR__.'/../../../../../../config.php');
 
 function get_object() {
     $obj = new stdClass();
     $obj->eventname = '';
-    $obj->component = ''; 
-    $obj->action = ''; 
-    $obj->target = ''; 
+    $obj->component = '';
+    $obj->action = '';
+    $obj->target = '';
     $obj->objecttable = '';
-    $obj->objectid = ''; 
-    $obj->crud = ''; 
-    $obj->edulevel = ''; 
-    $obj->contextid = ''; 
-    $obj->contextlevel = ''; 
-    $obj->contextinstanceid = ''; 
-    $obj->userid = ''; 
-    $obj->courseid = ''; 
-    $obj->relateduserid = ''; 
-    $obj->anonymous = ''; 
-    $obj->other = ''; 
-    $obj->timecreated = time(); 
-    $obj->origin = ''; 
-    $obj->ip = ''; 
+    $obj->objectid = '';
+    $obj->crud = '';
+    $obj->edulevel = '';
+    $obj->contextid = '';
+    $obj->contextlevel = '';
+    $obj->contextinstanceid = '';
+    $obj->userid = '';
+    $obj->courseid = '';
+    $obj->relateduserid = '';
+    $obj->anonymous = '';
+    $obj->other = '';
+    $obj->timecreated = time();
+    $obj->origin = '';
+    $obj->ip = '';
     $obj->realuserid = '';
     $obj->logstorestandardlogid = 0;
     $obj->type = 0;
