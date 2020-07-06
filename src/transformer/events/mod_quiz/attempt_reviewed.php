247b8d3032d1eb324bd0cdd134f4580de08907a6
László Záborski <laszlo.zaborski@learningpool.com>
2020-07-06 15:37:27 +0100

    LMS-2596 moodle-logstore_xapi: change JISC specific activity.

--- src/transformer/events/mod_quiz/attempt_reviewed.php
+++ src/transformer/events/mod_quiz/attempt_reviewed.php
@@ -42,7 +42,7 @@ function attempt_reviewed(array $config, \stdClass $event) {
 
     // Set JISC specific activity type.
     if (utils\is_enabled_config($config, 'send_jisc_data')) {
-        $object['definition']['type'] = 'http://xapi.jisc.ac.uk/activities/quiz';
+        $object = utils\get_activity\course_quiz($config, $course, $event->contextinstanceid);
     }
 
     return [[
