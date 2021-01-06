095ebe35fa44b2a1f1b78427cc059653817a3e7c
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-01-06 15:51:06 -0500

    Update version.php

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2018082100;
+$plugin->version = 2021010602;
 $plugin->release = '';
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
