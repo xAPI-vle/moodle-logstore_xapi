2a02d9ae9aecf352950c765801a77f34efdab567
Joel Smith <j.d.smith@cqu.edu.au>
2021-02-12 12:02:41 +1000

    Version bump

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2018082100;
-$plugin->release = '';
-$plugin->requires = 2018051700;
+$plugin->version = 2021021200;
+$plugin->release = 'v4.6.0';
+$plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_STABLE;
