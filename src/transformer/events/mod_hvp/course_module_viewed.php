807cba3f6a6f473d5260490d07142c7405fdb18f
Scott Verbeek <scottverbeek@catalyst-au.net>
2021-06-15 12:34:21 +1000

    Adding transformers for mod_hvp
    * Added transformer for course_module_viewed event from mod_hvp
    
    * Added transformer for course_module_instance_list_viewed event from mod_hvp
    
    * Added transformer for attempt_submitted event from mod_hvp

---
+++ src/transformer/events/mod_hvp/course_module_viewed.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * xAPI transformer for mod_hvp report viewed event.
+ *
+ * @package   logstore_xapi
+ * @copyright 2021 onwards Scott Verbeek <scottverbeek@catalyst-au.net>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace src\transformer\events\mod_hvp;
+
+defined('MOODLE_INTERNAL') || die();
+
+use src\transformer\utils as utils;
+
+function course_module_viewed(array $config, \stdClass $event) {
+    $repo = $config['repo'];
+    $user = $repo->read_record_by_id('user', $event->userid);
+    $course = $repo->read_record_by_id('course', $event->courseid);
+    $lang = utils\get_course_lang($course);
+
+    return [[
+        'actor' => utils\get_user($config, $user),
+        'verb' => [
+            'id' => 'http://id.tincanapi.com/verb/viewed',
+            'display' => [
+                $lang => 'viewed'
+            ],
+        ],
+        'object' => utils\get_activity\hvp($config, $event->contextinstanceid),
+        'timestamp' => utils\get_event_timestamp($event),
+        'context' => [
+            'platform' => $config['source_name'],
+            'language' => $lang,
+            'extensions' => utils\extensions\base($config, $event, $course),
+            'contextActivities' => [
+                'grouping' => [
+                    utils\get_activity\site($config),
+                    utils\get_activity\course($config, $course),
+                ],
+                'category' => [
+                    utils\get_activity\source($config),
+                ]
+            ],
+        ]
+    ]];
+}
---
+++ src/transformer/utils/get_activity/hvp.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace src\transformer\utils\get_activity;
+defined('MOODLE_INTERNAL') || die();
+
+use src\transformer\utils as utils;
+
+/**
+ * Return the object element in the xAPI call.
+ *
+ * @param array $config
+ * @param int $cmid
+ * @return array $object
+ */
+function hvp(array $config, $cmid) {
+    $lang = $config['source_lang'];
+    $repo = $config['repo'];
+    $xapitype = 'http://adlnet.gov/expapi/activities/module';
+
+    $coursemodule = $repo->read_record_by_id('course_modules', $cmid);
+    $module = $repo->read_record_by_id('modules', $coursemodule->module);
+    $instance = $repo->read_record_by_id($module->name, $coursemodule->instance);
+
+    $coursemoduleurl = $config['app_url'].'/mod/'.$module->name.'/view.php?id='.$cmid;
+    $instancename = property_exists($instance, 'name') ? $instance->name : 'H5P';
+
+    $object = [
+        'id' => $coursemoduleurl,
+        'definition' => [
+            'type' => $xapitype,
+            'name' => [
+                $lang => $instancename,
+            ],
+        ],
+    ];
+
+    if (utils\is_enabled_config($config, 'send_course_and_module_idnumber')) {
+        $moduleidnumber = property_exists($coursemodule, 'idnumber') ? $coursemodule->idnumber : null;
+        $object['definition']['extensions']['https://w3id.org/learning-analytics/learning-management-system/external-id'] =
+            $moduleidnumber;
+    }
+
+    return $object;
+}
+
+/**
+ * Return the object element in the xAPI call.
+ *
+ * @param array $config
+ * @param int $courseid
+ * @return array $object
+ */
+function hvp_instance_list(array $config, $courseid)
+{
+    $lang = $config['source_lang'];
+    $xapitype = 'http://adlnet.gov/expapi/activities/module';
+
+    $instancelisturl = $config['app_url'].'/mod/hvp/index.php?id='.$courseid;
+    $instancename = 'H5P';
+
+    return [
+        'id' => $instancelisturl,
+        'definition' => [
+            'type' => $xapitype,
+            'name' => [
+                $lang => $instancename,
+            ],
+        ],
+    ];
+}
