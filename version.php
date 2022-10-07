567e3d7c009e7e12e2ea5b21356024e2b4bb3670
David Pesce <davidpesce@gmail.com>
2022-10-07 16:04:19 -0400

    using latest version.php from CE version 4.6.0

--- version.php
+++ version.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2018082100;
-$plugin->release = '';
-$plugin->requires = 2018051700;
+$plugin->version = 2020032801;
+$plugin->release = 'v4.6.0';
+$plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_STABLE;
