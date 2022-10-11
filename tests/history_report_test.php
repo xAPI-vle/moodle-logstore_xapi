31ba848ad0c2c46214e2fc9c05bb36fd2548c983
David Pesce <davidpesce@gmail.com>
2022-10-11 11:40:46 -0400

    namespace fixes

--- tests/history_report_test.php
+++ tests/history_report_test.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace logstore_xapi\tests;
+namespace logstore_xapi;
 
 defined('MOODLE_INTERNAL') || die();
 
