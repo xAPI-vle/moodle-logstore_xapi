d598fe20bd772d433aae1eb16f5b33f721255e29
David Pesce <davidpesce@gmail.com>
2022-10-11 11:34:39 -0400

    namespace fixes

--- tests/xapi_test_case.php
+++ tests/xapi_test_case.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace tests;
+namespace logstore_xapi\tests;
 
 use \PHPUnit\Framework\TestCase as PhpUnitTestCase;
 use \Locker\XApi\Statement as LockerStatement;
