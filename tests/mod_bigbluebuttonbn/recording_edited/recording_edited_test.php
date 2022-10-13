81266beb58a9a1c260d7f25c6feb78f9cd6209ff
David Pesce <david.pesce@exputo.com>
2022-10-13 15:45:37 +0000

    attempt to load BBB plugin

--- tests/mod_bigbluebuttonbn/recording_edited/recording_edited_test.php
+++ tests/mod_bigbluebuttonbn/recording_edited/recording_edited_test.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace logstore_xapi\mod_bigbluebuttonbn\recording_deleted;
+namespace logstore_xapi\mod_bigbluebuttonbn\recording_edited;
 
 defined('MOODLE_INTERNAL') || die();
 
