041232101d77b237a2cdcdbdc6b761e7e2f0baf3
David Pesce <david.pesce@exputo.com>
2022-10-14 13:49:14 +0000

    add mod_feedback tests

--- tests/mod_feedback/response_submitted/multichoicerated/multichoicerated_test.php
+++ tests/mod_feedback/response_submitted/multichoicerated/multichoicerated_test.php
@@ -16,8 +16,20 @@
 
 namespace logstore_xapi\mod_feedback\response_submitted\multichoicerated;
 
-class test extends \logstore_xapi\xapi_test_case {
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/admin/tool/log/store/xapi/tests/xapi_test_case.php');
+
+class multichoicerated_test extends \logstore_xapi\xapi_test_case {
     protected function get_test_dir() {
         return __DIR__;
+    }
+
+    // Adding this to appease the auto-detecting of test cases.
+    // xapi_test_case has the default test cases and this test extends that.
+    public function test_init() {
+
     }
 }
--- tests/mod_feedback/response_submitted/unknown_typ/unknown_typ_test.php
+++ tests/mod_feedback/response_submitted/unknown_typ/unknown_typ_test.php
@@ -14,10 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace logstore_xapi\mod_feedback\response_submitted\multichoice;
+namespace logstore_xapi\mod_feedback\response_submitted\unknown_typ;
 
-class test extends \logstore_xapi\xapi_test_case {
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/admin/tool/log/store/xapi/tests/xapi_test_case.php');
+
+class unknown_typ_test extends \logstore_xapi\xapi_test_case {
     protected function get_test_dir() {
         return __DIR__;
+    }
+
+    // Adding this to appease the auto-detecting of test cases.
+    // xapi_test_case has the default test cases and this test extends that.
+    public function test_init() {
+
     }
 }
