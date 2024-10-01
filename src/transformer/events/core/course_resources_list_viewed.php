8236d05152263503b001dc133a73ce5c04c6cd46
Milton Reder <milt@yetanalytics.com>
2024-10-01 16:15:44 -0400

    first crack

---
+++ src/transformer/events/core/course_resources_list_viewed.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Transform for course resources list viewed event.
+ *
+ * @package   logstore_xapi
+ * @copyright Milt Reder <milt@yetanalytics.com>
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace src\transformer\events\core;
+
+use src\transformer\utils as utils;
+
+/**
+ * Transformer for course resources list viewed event.
+ *
+ * @param array $config The transformer config settings.
+ * @param \stdClass $event The event to be transformed.
+ * @return array
+ */
+function course_resources_list_viewed(array $config, \stdClass $event) {
+    $repo = $config['repo'];
+    $user = $repo->read_record_by_id('user', $event->userid);
+    $course = $repo->read_record_by_id('course', $event->courseid);
+    $lang = utils\get_course_lang($course);
+
+    return [[
+        'actor' => utils\get_user($config, $user),
+        'verb' => [
+            'id' => 'http://id.tincanapi.com/verb/viewed',
+            'display' => [
+                $lang => 'Viewed'
+            ],
+        ],
+        'object' => [
+            'id' => $config['app_url'] . '/course/resources.php?id=' . $event->courseid,
+            'definition' => [
+                'type' => 'https://w3id.org/xapi/acrossx/activities/webpage',
+                'name' => [
+                    $lang => $course->fullname . ' Resources'
+                ],
+            ],
+        ],
+        'context' => [
+            'language' => $lang,
+            'extensions' => utils\extensions\base($config, $event, $course),
+            'contextActivities' => [
+                'category' => [
+                    utils\get_activity\site($config)
+                ]
+            ],
+        ]
+    ]];
+}
