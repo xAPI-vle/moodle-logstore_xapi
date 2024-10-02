3801473922d3f38901a1dd36384f646d6369fb26
Milton Reder <milt@yetanalytics.com>
2024-10-02 10:55:08 -0400

    hack jisc test constants again

--- tests/enchancement_jisc_skeleton.php
+++ tests/enchancement_jisc_skeleton.php
@@ -42,9 +42,9 @@ abstract class enchancement_jisc_skeleton extends \advanced_testcase {
     protected $generatedhistorylog = 12;
 
     /**
-     * @var int Generated xapi-log events numbers
+     * @var int Generated xapi-log events number of statements per new course
      */
-    protected $generatedxapilog = 3;
+    protected $generatedxapilog = 9;
 
     /**
      * @var array Form defaults.
