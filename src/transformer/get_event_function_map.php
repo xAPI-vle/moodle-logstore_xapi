9870174d477ed2df0c85b0706079b2f729975d56
Milton Reder <milt@yetanalytics.com>
2024-10-02 15:59:23 -0400

    first crack

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -42,6 +42,7 @@ function get_event_function_map() {
 
         '\core\event\course_completed' => 'core\course_completed',
         '\core\event\course_viewed' => 'core\course_viewed',
+        '\core\event\group_created' => 'core\group_created',
         '\core\event\user_created' => 'core\user_created',
         '\core\event\user_enrolment_created' => 'core\user_enrolment_created',
         '\core\event\user_loggedin' => 'core\user_loggedin',
