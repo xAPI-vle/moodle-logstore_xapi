d6f5f3c9435a68e7dbbeeeb9021a00f07a7cad43
Milton Reder <milt@yetanalytics.com>
2024-10-08 16:13:02 -0400

    don't set in quiz except in jisc case

--- src/transformer/utils/get_activity/course_quiz.php
+++ src/transformer/utils/get_activity/course_quiz.php
@@ -37,12 +37,13 @@ use src\transformer\utils as utils;
  * @return array
  */
 function course_quiz(array $config, \stdClass $course, string $cmid) {
-    $xapytype = 'http://adlnet.gov/expapi/activities/assessment';
-
     // JISC specific activity type.
     if (utils\is_enabled_config($config, 'send_jisc_data')) {
         $xapytype = 'http://xapi.jisc.ac.uk/activities/quiz';
+        return utils\get_activity\course_module(
+            $config, $course, $cmid, 'http://xapi.jisc.ac.uk/activities/quiz'
+        );
+    } else {
+        return utils\get_activity\course_module($config, $course, $cmid);
     }
-
-    return utils\get_activity\course_module($config, $course, $cmid, $xapytype);
 }
