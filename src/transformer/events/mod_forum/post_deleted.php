6ef2e48955ba218d8ca360a39c1e87385ab0f72a
Cliff Casey <cliff@yetanalytics.com>
2024-10-21 12:27:07 -0400

    post delete

---
+++ src/transformer/events/mod_forum/post_deleted.php
@@ -0,0 +1,73 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Transform for the forum post created event.
+ *
+ * @package   logstore_xapi
+ * @copyright Cliff Casey <cliff@yetanalytics.com>
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace src\transformer\events\mod_forum;
+
+use src\transformer\utils as utils;
+
+/**
+ * Transformer for forum post deleted event.
+ *
+ * @param array $config The transformer config settings.
+ * @param \stdClass $event The event to be transformed.
+ * @return array
+ */
+function post_deleted(array $config, \stdClass $event) {
+    //debug
+    $repo = $config['repo'];
+    $user = $repo->read_record_by_id('user', $event->userid);
+    $course = $repo->read_record_by_id('course', $event->courseid);
+    $lang = utils\get_course_lang($course);
+    
+    $post = new \stdClass();
+    $post->id = $event->objectid;
+    
+    $other = unserialize($event->other);
+    $discussionid = $other['discussionid'];
+    $discussion = $repo->read_record_by_id('forum_discussions', $discussionid);
+
+    return [[
+        'actor' => utils\get_user($config, $user),
+        'verb' => [
+            'id' => 'http://activitystrea.ms/delete',
+            'display' => [
+                'en-US' => 'Deleted'
+            ],
+        ],
+        'object' => utils\get_activity\forum_discussion_post_reply($config, $course, $post),
+        'context' => [
+            'language' => $lang,
+            'extensions' => utils\extensions\base($config, $event, $course),
+            'contextActivities' => [
+                'parent' => array_merge(
+                    [utils\get_activity\course_discussion($config, $course, $discussion)], 
+                    utils\context_activities\get_parent($config, $event->contextinstanceid, true)
+                ),
+                'category' => [
+                    utils\get_activity\site($config),
+                ],
+            ],
+        ]
+    ]];
+}
--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -87,6 +87,9 @@ function get_event_function_map() {
         '\mod_forum\event\discussion_created' => 'mod_forum\discussion_created',
         '\mod_forum\event\discussion_viewed' => 'mod_forum\discussion_viewed',
         '\mod_forum\event\post_created' => 'mod_forum\post_created',
+
+        '\mod_forum\event\post_deleted' => 'mod_forum\post_deleted',
+
         '\mod_forum\event\user_report_viewed' => 'mod_forum\user_report_viewed',
         '\mod_glossary\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_imscp\event\course_module_viewed' => 'all\course_module_viewed',
