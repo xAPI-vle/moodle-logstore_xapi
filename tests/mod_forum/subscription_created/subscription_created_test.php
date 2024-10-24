b6954fe5c243cec36403c910a2d4751128148ed3
Cliff Casey <cliff@yetanalytics.com>
2024-10-24 14:45:12 -0400

    forum subscription tests

--- tests/mod_forum/subscription_created/subscription_created_test.php
+++ tests/mod_forum/subscription_created/subscription_created_test.php
@@ -31,7 +31,7 @@ require_once($CFG->dirroot . '/admin/tool/log/store/xapi/tests/xapi_test_case.ph
  *            David Pesce <david.pesce@exputo.com>
  * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class existing_module_test extends \logstore_xapi\xapi_test_case {
+class subscription_created_test extends \logstore_xapi\xapi_test_case {
 
     /**
      * Retrieve the directory of the unit test.
