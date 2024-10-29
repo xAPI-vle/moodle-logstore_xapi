9d2abd75794a7455ab1124500bd4d410def1c02f
Cliff Casey <cliff@yetanalytics.com>
2024-10-29 11:59:03 -0400

    simplify namespaces

--- tests/mod_forum/discussion_subcription_created/discussion_subscription_created_test.php
+++ tests/mod_forum/discussion_subcription_created/discussion_subscription_created_test.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace logstore_xapi\mod_forum\discussion_created;
+namespace logstore_xapi\mod_forum;
 
 defined('MOODLE_INTERNAL') || die();
 
