59a96000d9b23b0baf618001a22afa8b10a39d51
Daniel Bell <daniel@yetanalytics.com>
2024-11-08 14:50:22 -0600

    badge_viewed

---
+++ src/transformer/events/core/badge_viewed.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Transformer fn for badge_viewed event
+ *
+ * @package   logstore_xapi
+ * @copyright Daniel Bell <daniel@yetanalytics.com>
+ *            
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace src\transformer\events\core;
+
+use src\transformer\utils as utils;
+use src\transformer\utils\get_activity as activity;
+
+/**
+ * Transformer fn for badge_viewed event
+ *
+ * @param array $config The transformer config settings.
+ * @param \stdClass $event The event to be transformed.
+ * @return array
+ */
+
+function badge_viewed(array $config, \stdClass $event) {
+    global $CFG;
+    $repo = $config['repo'];
+    $badge = $repo->read_record_by_id('badge', $event->objectid);
+    $badgetype = [1 => "Global", 2 => "Course"][$badge->type];
+
+    //all three here may not exist
+    $user=$repo->read_record_by_id('user',$event->userid); 
+    $course = (isset($event->courseid) && $event->courseid != 0)
+        ? $repo->read_record_by_id('course', $event->courseid)
+        : null;
+    $lang = is_null($course) ? $config['source_lang'] : utils\get_course_lang($course);
+
+    $statement = [
+        'actor' => utils\get_user($config,$user),
+        'verb' => ['id' => 'http://id.tincanapi.com/verb/viewed',
+                   'display' => ['en' => 'Viewed']
+        ],
+        'object' => [
+          'id' => $config['app_url'].'/badges/overview.php?id='.$badge->id,
+          'definition' => [
+            'name' => [$lang =>$badge->name],
+            'description' => [$lang => $badge->description],
+            'type' =>   'https://xapi.edlm/profiles/edlm-lms/concepts/activity-types/badge',
+            'extensions' => [
+              'https://xapi.edlm/profiles/edlm-lms/v1/concepts/activity-extensions/badge-type' =>  $badgetype,
+              'https://xapi.edlm/profiles/edlm-lms/v1/concepts/activity-extensions/badge-version' => $badge->version
+            ]
+          ],
+        ],
+        'context' => [
+            'language' => $lang,
+            'contextActivities' =>  [
+                'category' => [activity\site($config)],
+            ],
+            'extensions' => utils\extensions\base($config, $event, $course)
+        ]];
+
+        if ($course){
+            $statement = utils\add_parent($config,$statement,$course);
+        }
+    
+        return [$statement];
+}
---
+++ tests/core/badge_viewed/user_viewed_badge/user_viewed_badge_test.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace logstore_xapi\core\badge_viewed\user_viewed_badge;
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/admin/tool/log/store/xapi/tests/xapi_test_case.php');
+
+/**
+ * Unit test for badge_viewed event
+ *
+ * @package   logstore_xapi
+ * @copyright Daniel Bell <daniel@yetanalytics.com>
+ * @license   https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class user_viewed_badge_test extends \logstore_xapi\xapi_test_case {
+
+    /**
+     * Retrieve the directory of the unit test.
+     *
+     * @return string
+     */
+    protected function get_test_dir() {
+        return __DIR__;
+    }
+
+    /**
+     * Retrieve the plugin type being tested.
+     *
+     * @return string
+     */
+    protected function get_plugin_type() {
+        return "core";
+    }
+    
+    /**
+     * Retrieve the plugin name being tested.
+     *
+     * @return string
+     */
+    protected function get_plugin_name() {
+        return "core";
+    }
+
+    /**
+     * Appease auto-detecting of test cases. xapi_test_case has default test cases.
+     *
+     * @covers ::attempt_submitted
+     * @return void
+     */
+    public function test_init() {
+
+    }
+}
