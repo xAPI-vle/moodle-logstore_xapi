464375bb679a04a94e338f6cc401ad5091f4f5c6
David <davidpesce@gmail.com>
2025-06-04 15:28:21 -0400

    fixed trailing comma

--- src/transformer/utils/get_activity/definition/cmi/fill_in.php
+++ src/transformer/utils/get_activity/definition/cmi/fill_in.php
@@ -52,7 +52,7 @@ function fill_in(
                         implode('[,]', $correctanswers),
                     ],
                 ]
-                : [],
+                : []
         ),
     ];
 }
